f0bf2efb5dcf866565f613026bc0b196f1d0ac7a
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-25 07:36:52 +0330

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,6 +2,6 @@
 
 mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db
 redis-server --daemonize yes
-cp .devcontainer/local_settings_example.py sefaria
+cp .devcontainer/local_settings.py sefaria
 npm install
 npm run setup
