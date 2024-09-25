b6fed70bf13fdf49a0df2393f128058c29f4680f
ORX AI <131006215+orxai@users.noreply.github.com>
2024-09-25 12:34:10 +0330

    Update postStart.sh

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+mkdir log && chmod 777 log
+mongod --fork --logpath /var/log/mongodb.log --dbpath /data/db
+redis-server --daemonize yes
+cp .devcontainer/local_settings.py sefaria
+npm install
+npm run setup
+npm run build-client
+npm run build
 /python3.8 manage.py migrate
 /python3.8 manage.py runserver
